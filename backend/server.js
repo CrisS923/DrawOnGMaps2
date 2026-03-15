e0c574c034b286cc46d0db7a7a9c185cd975f422
Cristian Niculae <cristian3.niculae@gmail.com>
2026-03-15 19:38:51 +0000

    add drawings API

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,42 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
+app.use(express.json());
+
+let drawings = [];
+
+// health check
 app.get("/", (req, res) => {
   res.send("DrawOnGMaps backend running");
+});
+
+// get all drawings
+app.get("/drawings", (req, res) => {
+  res.json(drawings);
+});
+
+// create drawing
+app.post("/drawings", (req, res) => {
+  const drawing = {
+    id: Date.now(),
+    ...req.body
+  };
+
+  drawings.push(drawing);
+  res.json(drawing);
+});
+
+// delete drawing
+app.delete("/drawings/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  drawings = drawings.filter(d => d.id !== id);
+
+  res.json({ deleted: id });
 });
 
 app.listen(PORT, () => {
